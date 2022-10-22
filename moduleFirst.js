e19c41ea17fe72a8d93093d8d11bb2d3c98632e6
Gourav <gkumars2956@gmail.com>
2022-10-22 15:11:19 +0530

    done url, pathmodules,mjsfile

---
+++ moduleFirst.js
@@ -0,0 +1,15 @@
+// const simple = require("./modulesecond")
+// import {simple2 as simple} from "./modulesecond.mjs"
+// simple()
+
+// import simple23 from "./modulesecond.mjs"
+
+// simple23()
+
+// import {simple, simple2}  from "./modulesecond.mjs"
+// simple()
+// simple2()
+
+import * as a2 from "./modulesecond.mjs"
+
+console.log(a2.simple())
