5db5824f7135ee0e7d24e75e7c34780e31809c8c
Gourav <gourav@LAPTOP-868QQ3N0.localdomain>
2022-10-23 22:55:28 +0530

    done the html part

--- Website.js
+++ Website.js
@@ -1,12 +1,27 @@
+
+// const {Console}=require('console');
 const http = require('http');
 const port = process.env.PORT ||3000;
 
 const server =http.createServer((req,res)=>{
-    console.log(req.url);
-    // console.log(req);
     res.statusCode =200;
     res.setHeader('Content-Type','text/html')
-    res.end('<h1>This is Gourav Kumar Shaw</h1><p>Hey this is the way to write in server!!</p>');
+    console.log(req.url);
+    // console.log(req);
+   
+    if(req.url=='/'){
+        res.statusCode =200;
+    res.end('<h1>This is Gourav Kumar Shaw</h1><p>Hey this is the way to write in server!!</p>');}
+    else if(req.url=='/about'){
+        res.statusCode =200;
+        res.end('<h1>This is Gourav Kumar Shaw</h1><p>Hey this is the way to write in server!!</p>');
+        
+    }
+    else   {
+        res.gourav();
+        res.statusCode =404;
+        res.end('<h1>Not found</h1><p>Hey this page was not found on this server</p>');
+        }
 })
 
 server.listen(port,()=>{
