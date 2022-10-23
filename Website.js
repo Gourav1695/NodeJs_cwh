47f37d7bae5bf661051a9917e51a71254064d29e
Gourav <gkumars2956@gmail.com>
2022-10-23 23:54:07 +0530

    done the tutorial :)

--- Website.js
+++ Website.js
@@ -1,6 +1,7 @@
 
 // const {Console}=require('console');
 const http = require('http');
+const fs = require('fs');
 const port = process.env.PORT ||3000;
 
 const server =http.createServer((req,res)=>{
@@ -14,11 +15,17 @@ const server =http.createServer((req,res)=>{
     res.end('<h1>This is Gourav Kumar Shaw</h1><p>Hey this is the way to write in server!!</p>');}
     else if(req.url=='/about'){
         res.statusCode =200;
-        res.end('<h1>This is Gourav Kumar Shaw</h1><p>Hey this is the way to write in server!!</p>');
+        res.end('<h1>Gourav Kumar Shaw</h1><p>Hey this is the way to write in server!!</p>');
+        
+    }
+    else if(req.url=='/hello'){
+        res.statusCode =200;
+        const data = fs.readFileSync('index.html');
+        res.end(data.toString());
         
     }
     else   {
-        res.gourav();
+        // res.gourav();
         res.statusCode =404;
         res.end('<h1>Not found</h1><p>Hey this page was not found on this server</p>');
         }
